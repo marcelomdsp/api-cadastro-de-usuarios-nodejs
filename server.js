c2bc0fa909ab58decc92062ceac5051b0acd68b7
marcelomdsp <marcelopaulomsp@gmail.com>
2024-11-05 23:25:08 -0300

    Configuraçao da porta para vercel

--- server.js
+++ server.js
@@ -76,6 +76,8 @@ app.delete("/usuarios/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
